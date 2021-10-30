98f82ba2d6eae49cdeed935f89ff27fe7a10adc4
eugeniuszVI96 <93393687+eugeniuszVI96@users.noreply.github.com>
2021-10-30 15:05:24 +0200

    Zadanie: pierwszy test jednostkowy (#3)

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,23 +1,33 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
 import com.kodilla.testing.user.SimpleUser;
 
 public class TestingMain {
 
     public static void main(String[] args) {
+        System.out.println("Testing SimpleUser class");
+
         SimpleUser simpleUser = new SimpleUser("theForumUser");
 
         String result = simpleUser.getUsername();
-
         if (result.equals("theForumUser")) {
             System.out.println("test OK");
         } else {
             System.out.println("Error!");
         }
 
-        // tu umieść kolejny test ->
-        System.out.println("Test - pierwszy test jednostkowy:");
+        System.out.println("Testing Calculator class");
 
-        //...
+        Calculator calculator = new Calculator();
+
+        int addResult = calculator.add(180, 19);
+        int subResult = calculator.subtract(200, 150);
+
+        if ((addResult == 199) && (subResult == 50)) {
+            System.out.println("Calculator test OK");
+        } else {
+            System.out.println("Calculator has error!");
+        }
     }
 }
