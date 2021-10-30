ad2dd1c9b36f4ed19b73c9d486908ccfb4234694
Yevhenii Yablonskyi <yevhenii.yablonskyi@gmail.com>
2021-10-30 20:08:48 +0200

    Zadanie: test klasy wybierającej parzyste liczby z kolekcji

---
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -0,0 +1,60 @@
+package com.kodilla.testing.collection;
+
+import org.junit.jupiter.api.*;
+import java.util.*;
+
+@DisplayName("Collection Test Suite")
+public class CollectionTestSuite {
+
+    @BeforeEach
+    public void beforeEach() {
+        System.out.println("Test case: start");
+    }
+
+    @AfterEach
+    public void afterEach() {
+        System.out.println("Test case: end");
+    }
+
+    @BeforeAll
+    public static void beforeAll() {
+        System.out.println("Test suite: start");
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        System.out.println("Test suite: end");
+    }
+
+    @DisplayName("When the list is empty it doesn't appear error")
+    @Test
+    void testOddNumberExterminatorEmptyList() {
+        //Given
+        List<Integer> emptyList = new ArrayList<>();
+
+        OddNumberExterminator oddNumberExterminator = new OddNumberExterminator();
+
+
+        //When
+        List<Integer> result = oddNumberExterminator.exterminate(emptyList);
+
+        //Then
+        Assertions.assertEquals(result, emptyList);
+    }
+
+    @DisplayName("When the list is filled numbers odd and even it doesn't appear error")
+    @Test
+    void testOddNumbersExterminatorNormalList() {
+        //Given
+        List<Integer> listDifferentNumbers = new ArrayList<>();
+        Arrays.asList(1, 2);
+
+        OddNumberExterminator oddNumberExterminator = new OddNumberExterminator();
+
+        //When
+        List<Integer> result = oddNumberExterminator.exterminate(listDifferentNumbers);
+
+        //Then
+        Assertions.assertEquals(result, listDifferentNumbers);
+    }
+}
