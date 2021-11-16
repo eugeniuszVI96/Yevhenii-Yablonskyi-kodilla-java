0b3f1cc3964e192bdd8b5e5f777582ad7e475a1a
eugeniuszVI96 <93393687+eugeniuszVI96@users.noreply.github.com>
2021-11-16 23:39:58 +0100

    Zadanie: rozbudowa WeatherForecastTestSuite (#13)
    * Zadanie: rozbudowa WeatherForecastTestSuite
    
    * Dodanie adnotacji
    
    * Dodanie testów
    
    * Dodanie zmian po uwagach 15.11

--- kodilla-testing/src/main/java/com/kodilla/testing/weather/stub/WeatherForecast.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/weather/stub/WeatherForecast.java
@@ -1,9 +1,9 @@
 package com.kodilla.testing.weather.stub;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class WeatherForecast {
+
     private Temperatures temperatures;
 
     public WeatherForecast(Temperatures temperatures) {
@@ -13,13 +13,65 @@ public class WeatherForecast {
     public Map<String, Double> calculateForecast() {
         Map<String, Double> resultMap = new HashMap<>();
 
-        for (Map.Entry<String, Double> temperature :
-                temperatures.getTemperatures().entrySet()) {
-
-            // adding 1 celsius degree to current value
-            // as a temporary weather forecast
-            resultMap.put(temperature.getKey(), temperature.getValue() + 1.0); // [1]
+        for (Map.Entry<String, Double> temperature : temperatures.getTemperatures().entrySet()) {
+            resultMap.put(temperature.getKey(), temperature.getValue() + 1.0);
         }
         return resultMap;
+    }
+
+    public Double calculateAverageTemperature() {
+
+        double result = 0;
+        if (temperatures.getTemperatures().size() == 0) {
+            return Double.NaN;
+        }
+        else {
+            for (Map.Entry<String, Double> temperature: temperatures.getTemperatures().entrySet()) {
+                result = result + temperature.getValue();
+            }
+            return result / temperatures.getTemperatures().size();
+        }
+    }
+
+    public Double calculateMedianTemperatures() {
+
+        double median = 0;
+        List<Double> temperaturesList = new ArrayList<>(temperatures.getTemperatures().values());
+        if (temperaturesList.size() == 0) {
+            return Double.NaN;
+        }
+        else {
+            Collections.sort(temperaturesList);
+
+            int halfSize = temperaturesList.size() / 2;
+            if (temperaturesList.size() % 2 == 0) {
+                median = (temperaturesList.get(halfSize) + temperaturesList.get(halfSize - 1)) / 2;
+            } else {
+                median = temperaturesList.get(halfSize);
+            }
+            return median;
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        WeatherForecast that = (WeatherForecast) o;
+
+        return temperatures.equals(that.temperatures);
+    }
+
+    @Override
+    public int hashCode() {
+        return temperatures.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "WeatherForecast{" +
+                "temperatures=" + temperatures +
+                '}';
     }
 }
