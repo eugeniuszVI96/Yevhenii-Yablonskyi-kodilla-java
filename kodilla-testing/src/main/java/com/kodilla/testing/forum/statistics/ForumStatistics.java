95c20f3be0671eaf195c2058ba07b34ac3f218bd
eugeniuszVI96 <93393687+eugeniuszVI96@users.noreply.github.com>
2021-11-18 01:16:44 +0100

    Małe zmiany (#16)

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/ForumStatistics.java
@@ -0,0 +1,23 @@
+package com.kodilla.testing.forum.statistics;
+
+import java.util.List;
+
+public class ForumStatistics {
+
+
+    //tóra obliczy podane powyżej wartości i zapamięta je we właściwościach (polach) klasy.
+
+    //liczbę użytkowników,
+    //liczbę postów,
+    //liczbę komentarzy,
+    //średnią liczbę postów na użytkownika,
+    //średnią liczbę komentarzy na użytkownika,
+    //średnią liczbę komentarzy na post.
+    public void calculateAdvStatistics(Statistics statistics) {
+
+    }
+
+    public List<Boolean> showStatistics() {
+        return null;
+    }
+}
---
+++ kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/ForumStatisticsTestSuite.java
@@ -0,0 +1,14 @@
+package com.kodilla.testing.forum.statistics;
+
+public class ForumStatisticsTestSuite {
+
+    //@BeforeEach
+
+    //gdy liczba postów = 0,
+    //gdy liczba postów = 1000,
+    //gdy liczba komentarzy = 0,
+    //gdy liczba komentarzy < liczba postów,
+    //gdy liczba komentarzy > liczba postów,
+    //gdy liczba użytkowników = 0,
+    //gdy liczba użytkowników = 100.
+}
