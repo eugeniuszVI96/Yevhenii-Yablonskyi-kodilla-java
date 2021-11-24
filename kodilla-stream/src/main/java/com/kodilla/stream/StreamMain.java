feb9aed60b326d584e7738339e5d0c64a1488f1c
Yevhenii Yablonskyi <yevhenii.yablonskyi.kodilla@gmail.com>
2021-11-24 19:02:56 +0100

    7.1 Wstęp do programowania funkcyjnego, czyli wyrażenia lambda

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -0,0 +1,28 @@
+package com.kodilla.stream;
+
+import com.kodilla.stream.lambda.*;
+import com.kodilla.stream.reference.FunctionalCalculator;
+
+public class StreamMain {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to module 7 - Stream");
+
+        Processor processor = new Processor();
+        processor.execute(() -> System.out.println("This is an example text"));
+
+        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+
+        System.out.println("Calculating expressions with lambdas");
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
+
+        System.out.println("Calculating expressions with method references");
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
+    }
+}
