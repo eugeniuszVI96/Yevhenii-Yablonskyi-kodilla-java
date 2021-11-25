b687a3dbbb3f51dd2adb06959ee4211a23a0b476
Yevhenii Yablonskyi <yevhenii.yablonskyi.kodilla@gmail.com>
2021-11-25 16:42:06 +0100

    7.2. Zadanie: niemutowalny użytkownik forum -

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -34,7 +34,5 @@ public class StreamMain {
         poemBeautifier.beautify("Text", text -> text.toLowerCase());
         poemBeautifier.beautify("Text", text -> text.toUpperCase());
         poemBeautifier.beautify("Text", text -> "\u001B[31m" + text + "\u001B[0m");
-
-
     }
 }
