b6a18e94c9348b8afba4f08eaa471f1cc78d524f
Yevhenii Yablonskyi <yevhenii.yablonskyi.kodilla@gmail.com>
2021-12-29 01:22:42 +0100

    +test dla pustej tablicy; +Double.NaN; -getAsDouble()

--- kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -1,17 +1,19 @@
 package com.kodilla.stream.array;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public interface ArrayOperations {
 
     static double getAverage(int[] numbers) {
         IntStream.range(0, numbers.length)
-              .map(n -> numbers[n])
-              .forEach(System.out::println);
+                .map(n -> numbers[n])
+                .forEach(System.out::println);
 
         return IntStream.range(0, numbers.length)
-              .map(n -> numbers[n])
-              .average().
-              getAsDouble();
+                .map(n -> numbers[n])
+                .average()
+                .orElse(Double.NaN);
     }
 }
