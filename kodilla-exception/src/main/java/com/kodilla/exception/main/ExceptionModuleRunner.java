11db9e4444cf6787dd51bd85574e734b3f5c162b
eugeniuszVI96 <93393687+eugeniuszVI96@users.noreply.github.com>
2022-01-18 20:22:54 +0100

    8.3. Throw i Throws – są jakieś różnice? (#33)

--- kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
@@ -1,11 +1,26 @@
 package com.kodilla.exception.main;
 
 import com.kodilla.exception.io.FileReader;
+import com.kodilla.exception.io.FileReaderException;
+import com.kodilla.exception.io.FileReaderWithoutHandling;
 
 public class ExceptionModuleRunner {
 
+    //FileReader
     public static void main(String[] args) {
         FileReader fileReader = new FileReader();
-        fileReader.readFile();
+
+        try {
+            fileReader.readFile("names.txt");
+        }
+        catch (FileReaderException e) {
+            System.out.println("Problem while reading a file!");
+        }
     }
+
+//    //FileReaderWithoutHandling
+//    public static void main(String[] args) {
+//        FileReaderWithoutHandling fileReader = new FileReaderWithoutHandling();
+//        fileReader.readFile();
+//    }
 }
