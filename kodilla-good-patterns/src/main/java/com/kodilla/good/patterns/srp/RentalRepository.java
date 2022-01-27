38a5260de58054a1714f6ea95ee32fb926678512
Yevhenii Yablonskyi <yevhenii.yablonskyi.kodilla@gmail.com>
2022-01-27 01:19:34 +0100

    13.2. SOLID i DRY

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/srp/RentalRepository.java
@@ -0,0 +1,8 @@
+package com.kodilla.good.patterns.srp;
+
+import java.time.LocalDateTime;
+
+public interface RentalRepository {
+
+    boolean createRental(User user, LocalDateTime from, LocalDateTime to);
+}
