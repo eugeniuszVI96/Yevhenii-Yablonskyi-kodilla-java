b0c0e674fdfc163d79c43dda366e75bfe3892a05
Yevhenii Yablonskyi <yevhenii.yablonskyi.kodilla@gmail.com>
2022-02-12 04:19:49 +0100

    14.3. Konfiguracja przy pomocy klasy Java

--- kodilla-spring/src/test/java/com/kodilla/spring/library/LibraryTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/library/LibraryTestSuite.java
@@ -1,10 +1,13 @@
 package com.kodilla.spring.library;
 
+import com.kodilla.spring.reader.ReaderConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.Arrays;
 
 @SpringBootTest
 public class LibraryTestSuite {
@@ -35,5 +38,18 @@ public class LibraryTestSuite {
         //Then
         //do nothing
     }
+
+    @Test
+    void testContext() {
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+
+        //When & Then
+        System.out.println("===== Beans list: =====");
+        Arrays.stream(context.getBeanDefinitionNames())
+                .forEach(System.out::println);
+        System.out.println("===== Beans list =====");
+    }
+
 
 }
