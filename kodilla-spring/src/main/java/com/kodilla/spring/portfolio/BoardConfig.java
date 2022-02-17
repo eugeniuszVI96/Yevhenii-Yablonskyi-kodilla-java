149d766bf706c8165627009cfa1a59915aaaec39
Yevhenii Yablonskyi <yevhenii.yablonskyi.kodilla@gmail.com>
2022-02-17 13:00:22 +0100

    14.3. zmainy

--- kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
@@ -1,33 +1,29 @@
 package com.kodilla.spring.portfolio;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import java.util.ArrayList;
-import java.util.List;
 
 @Configuration
 public class BoardConfig {
 
 
     @Bean
-    public TaskList taskList1() {
+    public TaskList toDoList() {
         return new TaskList();
     }
 
     @Bean
-    public TaskList taskList2() {
+    public TaskList inProgressList() {
         return new TaskList();
     }
 
     @Bean
-    public TaskList taskList3() {
+    public TaskList doneList() {
         return new TaskList();
     }
 
     @Bean
     public Board board() {
-        return new Board(taskList1(), taskList2(), taskList3());
+        return new Board(toDoList(), inProgressList(), doneList());
     }
 }
