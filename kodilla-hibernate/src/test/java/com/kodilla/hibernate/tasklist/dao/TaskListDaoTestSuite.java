9eb69badec50fd201f03c0edc33a638cede7a33c
eugeniuszVI96 <93393687+eugeniuszVI96@users.noreply.github.com>
2022-04-20 22:14:21 +0200

    17.2 Zadanie (#70)

---
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/tasklist/dao/TaskListDaoTestSuite.java
@@ -0,0 +1,35 @@
+package com.kodilla.hibernate.tasklist.dao;
+
+import com.kodilla.hibernate.tasklist.TaskList;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class TaskListDaoTestSuite {
+
+    @Autowired
+    private TaskListDao taskListDao;
+
+    @Test
+    void testFindByListName() {
+
+        //Given
+        TaskList taskList = new TaskList("List1", "Description1");
+        taskListDao.save(taskList);
+        String listName = taskList.getListName();
+
+        //When
+        List<TaskList> readTaskList = taskListDao.findByListName(listName);
+
+        //Then
+        assertEquals("List1", readTaskList.get(0).getListName());
+
+        //CleanUp
+        //taskListDao.delete(taskList);
+    }
+}
